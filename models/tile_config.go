8c7e55c847f6e1b96f4bb199cc06728e9a0c9e5e
Mojammel Haque Bhuiyan <mhbhuiyan10023@gmail.com>
2026-08-13 18:39:12 +0600

    feat: add tile configuration

--- models/tile_config.go
+++ models/tile_config.go
@@ -1,1 +1,14 @@
 package models
+
+type TileConfig struct {
+	Keyword      string                 `json:"keyword"`
+	PT           string                 `json:"pt"`
+	Amenities    string                 `json:"amenities"`
+	TilesPerPage int                    `json:"tiles_per_page"`
+	TotalTiles   int                    `json:"total_tiles"`
+	Order        string                 `json:"order"`
+	Mode         map[string]interface{} `json:"mode"`
+	TilesBlockID string                 `json:"tiles_block_id"`
+	Layout       string                 `json:"layout"`
+	Properties   []interface{}          `json:"properties"`
+}
